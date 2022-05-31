0016a8a194816b106ec6dc52e104de27c42aaa40
Will Hughes <willhughes01@gmail.com>
2022-05-30 23:01:58 -0700

    Super awesome inner api using RefCell
    Summary: I can have my cake and eat it too!  RefCell allows us to play a
    bit fast and loose with the borrowing rules, so we can have a nice easy
    external API while modifying internal pieces through refcell usage.

--- src/bin/demo/manual.rs
+++ src/bin/demo/manual.rs
@@ -1,5 +1,6 @@
-
 // A manual definition for what I want to generate
+
+use std::{cell::RefCell, rc::Rc};
 
 #[derive(Debug, Clone)]
 enum PluginError {
@@ -7,32 +8,42 @@ enum PluginError {
 }
 
 trait Plugin {
-    fn generate(&self, schemas: &[Schema]) -> Result<(), PluginError>;
+    fn generate(&self, schemas: &[Rc<Schema>]) -> Result<(), PluginError>;
 }
 
-struct DemoPlugin {
-}
+struct DemoPlugin {}
 
 impl Plugin for DemoPlugin {
-    fn generate(&self, schemas: &[Schema]) -> Result<(), PluginError> {
-        schemas.iter().try_for_each(|schema| -> Result<(), PluginError> {
-            println!("Schema: {}", schema.name);
-            schema.models.iter().try_for_each(|model| -> Result<(), PluginError> {
-                println!("Model: {}", model.name);
-                model.fields.iter().try_for_each(|field| -> Result<(), PluginError> {
-                    if field.name.starts_with('_') {
-                        return Err(PluginError::ValidationError(format!("Field {} starts with an underscore", field.name)));
-                    }
-                    println!("Field: {} {}", field.name, field.ttype);
-                    Ok(())
-                })
+    fn generate(&self, schemas: &[Rc<Schema>]) -> Result<(), PluginError> {
+        schemas
+            .iter()
+            .try_for_each(|schema| -> Result<(), PluginError> {
+                println!("Schema: {}", schema.name);
+                schema
+                    .models
+                    .borrow()
+                    .iter()
+                    .try_for_each(|model| -> Result<(), PluginError> {
+                        println!("Model: {}", model.name);
+                        model.fields.borrow().iter().try_for_each(
+                            |field| -> Result<(), PluginError> {
+                                if field.name.starts_with('_') {
+                                    return Err(PluginError::ValidationError(format!(
+                                        "Field {} starts with an underscore",
+                                        field.name
+                                    )));
+                                }
+                                println!("Field: {} {}", field.name, field.ttype);
+                                Ok(())
+                            },
+                        )
+                    })
             })
-        })
     }
 }
 
 struct Builder {
-    schemas: Vec<Schema>,
+    schemas: Vec<Rc<Schema>>,
     plugins: Vec<Box<dyn Plugin>>,
 }
 
@@ -44,11 +55,17 @@ impl Builder {
         }
     }
 
-    fn add_schema(&mut self, schema: Schema) {
-        self.schemas.push(schema);
+    fn new_schema(&mut self, name: &str) -> Rc<Schema> {
+        let schema = Rc::new(Schema {
+            name: name.to_string(),
+            models: RefCell::new(Vec::new()),
+        });
+        self.schemas.push(schema.clone());
+        schema
     }
 
     fn build(self) {
+        // TODO Flatten the schemas struct (should all be generated anyway)
         for plugin in self.plugins {
             plugin.generate(&self.schemas).unwrap();
         }
@@ -57,85 +74,85 @@ impl Builder {
 
 struct Schema {
     name: String,
-    models: Vec<Model>,
+    models: RefCell<Vec<Rc<Model>>>,
 }
 
 impl Schema {
-    fn new(name: &str) -> Self {
-        Self {
+    fn new_model(&self, name: &str) -> Rc<Model> {
+        let model = Rc::new(Model {
             name: name.to_string(),
-            models: Vec::new(),
-        }
-    }
-
-    fn add_model(&mut self, model: Model)  {
-        self.models.push(model);
+            fields: RefCell::new(Vec::new()),
+        });
+        self.models.borrow_mut().push(model.clone());
+        model
     }
 }
 
 struct Model {
     name: String,
-    fields: Vec<Field>,
+    fields: RefCell<Vec<Rc<Field>>>,
 }
 
 impl Model {
-    fn new(name: &str) -> Self {
-        Self {
-            name: name.to_string(),
-            fields: Vec::new(),
-        }
+    fn new_int(&self, name: &str) -> Rc<Field> {
+        let field = Rc::new(Field::new(name, "int64"));
+        self.fields.borrow_mut().push(field.clone());
+        field
     }
 
-    fn add_field(&mut self, field: Field) {
-        self.fields.push(field);
+    fn new_string(&self, name: &str) -> Rc<Field> {
+        let field = Rc::new(Field::new(name, "String"));
+        self.fields.borrow_mut().push(field.clone());
+        field
+    }
+
+    fn new_datetime(&self, name: &str) -> Rc<Field> {
+        let field = Rc::new(Field::new(name, "DateTime"));
+        self.fields.borrow_mut().push(field.clone());
+        field
     }
 }
 
 struct Field {
     name: String,
     ttype: String,
+    inner: RefCell<FieldMut>,
+}
+
+struct FieldMut {
+    desc: String,
 }
 
 impl Field {
-    fn new_int(name: &str) -> Self {
+    fn new(name: &str, ttype: &str) -> Self {
         Self {
             name: name.to_string(),
-            ttype: "int64".to_string(),
+            ttype: ttype.to_string(),
+            inner: RefCell::new(FieldMut {
+                desc: "".to_string(),
+            }),
         }
     }
 
-    fn new_string(name: &str) -> Self {
-        Self {
-            name: name.to_string(),
-            ttype: "String".to_string(),
-        }
-    }
-
-    fn new_datetime(name: &str) -> Self {
-        Self {
-            name: name.to_string(),
-            ttype: "DateTime".to_string(),
-        }
+    fn desc(&self, desc: &str) -> &Field {
+        self.inner.borrow_mut().desc = desc.to_string();
+        self
     }
 }
 
 pub fn demo() {
     let mut builder = Builder::new();
 
-    let mut schema = Schema::new("discordbot");
+    let schema = builder.new_schema("discordbot");
 
-    let mut alarm = Model::new("scheduled_alarm");
-    alarm.add_field(Field::new_int("id"));
-    alarm.add_field(Field::new_int("requester_discord_id"));
-    alarm.add_field(Field::new_int("channel_id"));
-    alarm.add_field(Field::new_int("hour_of_day"));
-    alarm.add_field(Field::new_int("day_of_week"));
-    alarm.add_field(Field::new_datetime("created_at"));
-    alarm.add_field(Field::new_string("message"));
-
-    schema.add_model(alarm);
-
-    builder.add_schema(schema);
+    let alarm = schema.new_model("scheduled_alarm");
+    alarm.new_int("id").desc("desc");
+    alarm.new_int("requester_discord_id").desc("desc");
+    alarm.new_int("channel_id").desc("desc");
+    alarm.new_int("hour_of_day").desc("desc");
+    alarm.new_int("day_of_week").desc("desc");
+    alarm.new_datetime("created_at").desc("desc");
+    alarm.new_string("message").desc("desc");
 
     builder.build();
 }
