98501ca30c02acde9ac7105168ee3e989eec9b72
Will Hughes <willhughes01@gmail.com>
2022-05-31 23:04:43 -0700

    Add full non-generated version of the configger generators
    Summary: Went through and wrote out almost exactly what I'd like to be
    able to generate from the field plugin and builder definitions.  There's
    probably still some kinks to iron out, but I think it's a solid
    foundation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
 pub use configger_derive::*;
 
-pub struct ConfiggerField {
-    pub name: &'static str,
-    // pub ty: &'static str, // TODO Syn type?
-    pub require_on_create: bool,
+#[derive(Debug, Clone)]
+pub enum PluginError {
+    ValidationError(String),
+}
+
+pub enum ConfiggerField {
+    Leaf {
+        name: &'static str,
+        // pub ty: &'static str, // TODO Syn type?
+        require_on_create: bool,
+    },
+    Node {
+        name: &'static str,
+        is_list: bool,
+        data: Vec<ConfiggerField>,
+    },
 }
 
 pub trait ConfiggerData {
